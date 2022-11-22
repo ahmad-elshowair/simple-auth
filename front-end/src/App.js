5ff94bfcdef207f9073f705feb0ac3e033c1dd38
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2022-11-22 21:03:21 +0700

    fix the miss spelling of importing BrowserRouter

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRoute, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './Components/Login';
 import { Dashboard } from './Components/Dashboard';
 import { Register } from './Components/Register';
@@ -7,13 +7,13 @@ import { Register } from './Components/Register';
 function App() {
   return (
     <>
-      <BrowserRoute>
+      <BrowserRouter>
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/register' element={<Register />} />
         </Routes>
-      </BrowserRoute>
+      </BrowserRouter>
     </>
   );
 }
