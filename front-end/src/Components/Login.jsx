bd34dd15f9c17f1d51212d0bcf808969e771c66a
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2022-11-25 22:53:16 +0700

    modify login component with structure form

--- front-end/src/Components/Login.jsx
+++ front-end/src/Components/Login.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export const Login = ({ setAuth }) => {
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={() => setAuth(true)}>Authenticate</button>
-    </div>
+    <>
+      <section className="login-section">
+        <article className="container">
+          <h1 className='mb-5 text-center'>Login</h1>
+          <form action="">
+            <div className="mb-3">
+              <input
+                type="email"
+                name="userEmail"
+                id="userEmail"
+                className="form-control"
+                placeholder='email ...'
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                name="userPassword"
+                id="userPassword"
+                className="form-control"
+                placeholder='password ...'
+              />
+            </div>
+            <div className="d-grid gap-2">
+              <button
+                className='btn btn-primary'
+                onClick={() => setAuth(true)}
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </article>
+      </section>
+    </>
   )
 }
